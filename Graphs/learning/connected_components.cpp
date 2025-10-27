2beec901aa9bd39684ccc8cdef7087d4bae12118
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-10-27 23:47:10 +0530

    just creating new folder

---
+++ Graphs/learning/connected_components.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//suppose graph is broken into non connected components then we use a vivited array 
+//vector<bool> arr(n+1,false); n--number of nodes
+//then we mark a node true in vivited if visited then we go to each node and check whether vivsted or not
+
+int main(){
+
+}
---
+++ Graphs/learning/intro.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// CYCLE - start and end at the same node
+// undirected cyclic graph (has a cycle)
+// directed acyclic graph (DAG) (no cycle)
+
+// PATH - contain a lot of node but each are reachable (node cannot appear twice in path)
+
+// DEGREE - number of edges that go or come out
+// total degree of a graph = 2* number of edges in a graph
+// Directed graph -> (Indegree, Outdegree)
+
+// Edge weight - every edge has a weight
+// by default assume unit weight
+
+
+
+int main()
+{
+    
+}

ca871bb36341a7fc6d2d6c28e88f5f285e4d31ee
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-10-29 00:52:03 +0530

    adding new method for flood fill problem

--- Graphs/BFS-DFS/floodFill.cpp
+++ Graphs/BFS-DFS/floodFill.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-//my solution
+// my solution
+// done by BFS
 vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
 {
     int m = image.size();
@@ -51,7 +51,46 @@ vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int co
     return image;
 }
 
+// striver solution
+// done by DFS
+//  The 'iniColor' parameter is added to the function signature based on its usage
+//  within the function and how it's called from the floodFill method.
+void dfs(int row, int col, vector<vector<int>> &ans,vector<vector<int>> &image, int newColor, int delRow[],int delCol[], int iniColor)
+{
+    ans[row][col] = newColor;
+    int n = image.size();
+    int m = image[0].size();
+
+    for (int i = 0; i < 4; i++)
+    {
+        int nrow = row + delRow[i];
+        int ncol = col + delCol[i];
+
+        // The condition 'ans[nrow][ncol] != newColor' is a logical completion
+        // of the partially visible line in the image.
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+            image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor)
+        {
+            dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);
+        }
+    }
+}
+
+vector<vector<int>> floodFill(vector<vector<int>> &image,int sr, int sc, int newColor)
+{
+    int iniColor = image[sr][sc];
+    vector<vector<int>> ans = image;
+    int delRow[] = {-1, 0, +1, 0};
+    int delCol[] = {0, +1, 0, -1};
+
+    // Note: A common edge case to handle is `if (iniColor == newColor) return image;`
+    // to prevent unnecessary computation or infinite loops, though it's not in the original image.
+
+    dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
+    return ans;
+}
+
 int main()
 {
-
+    
 }

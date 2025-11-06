63704ef628e754280a5025d720dfaf0b71f3c480
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-11-07 00:48:15 +0530

    adding DFS method in detect Bipartite graph question

--- Graphs/BFS-DFS/detect_Bipartite.cpp
+++ Graphs/BFS-DFS/detect_Bipartite.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+//ME
 bool BFS(vector<vector<int>> &adjList,int i,vector<pair<bool,int>> &visited,int group)
 {
     //U -- group=0
@@ -40,6 +42,64 @@ bool detectBipartite(vector<vector<int>> &adjList,int n)
                 return false;
     return true;
 }
+
+
+//STRIVER's
+bool BFS_Helper(vector<vector<int>> &adjList,int node,vector<int> &visited)
+{
+    queue<int> q;
+    q.push(node);
+    visited[node]=0;
+    while(!q.empty())
+    {
+        int i=q.front();
+        q.pop();
+        int GroupToBeAssigned=1-visited[i];
+        for(int &x:adjList[i])
+        {
+            if(visited[x]==-1)
+            {
+                q.push(x);
+                visited[x]=GroupToBeAssigned;
+            }
+            else if(visited[x]==visited[i])
+                return false;
+        }
+    }
+    return true;
+}
+bool DFS_Helper(vector<vector<int>> &adjList,int node,vector<int> &visited,int color)
+{
+    visited[node]=color;
+    for(int &x:adjList[node])
+    {
+        if(visited[x]==-1)
+        {
+            visited[x]=!color;
+            if(!DFS_Helper(adjList,x,visited,!color))
+                return false;
+        }
+        else if(visited[x]==visited[node])
+            return false;
+    }
+    return true;
+}
+bool detectBipartite_BFS_OR_DFS(vector<vector<int>> &adjList)
+{
+    int n=adjList.size();
+    vector<int> visited(n,-1);
+    
+    for(int i=0;i<n;i++)
+        if(visited[i]==-1)
+            if(!BFS_Helper(adjList,i,visited))
+                return false;
+    return true;
+}
+
+
+
+
+
 
 
 

0c82ff339caee7a4163af628424f9bf5ceaa9460
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-11-07 13:10:39 +0530

    adding question leetcode 210 Course Scheduler 2

---
+++ Graphs/TopoSort/courseScheduler2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites)
+{
+    vector<vector<int>> adjList(numCourses);
+    for(int i=0;i<prerequisites.size();i++)
+    {
+        adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+    }
+    int n=adjList.size();
+    vector<int> inDegree(n,0);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<adjList[i].size();j++)
+            inDegree[adjList[i][j]]++;
+    queue<int> q;
+    for(int i=0;i<n;i++)
+        if(inDegree[i]==0)
+            q.push(i);
+    vector<int> ans;
+    while(!q.empty())
+    {
+        int node=q.front();
+        q.pop();
+        ans.push_back(node);
+        for(int &x:adjList[node])
+        {
+            inDegree[x]--;
+            if(inDegree[x]==0)
+                q.push(x);
+        }
+    }
+    if(ans.size()!=n)
+        return {};
+    return ans;       
+}
+
+
+int main()
+{
+
+}

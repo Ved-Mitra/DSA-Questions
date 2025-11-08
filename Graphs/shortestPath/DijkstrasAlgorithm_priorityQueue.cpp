0c03cd0c9e0e48a6a86e4c19eb675f5e6b0c08e1
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-11-08 16:24:11 +0530

    adding TC derivation for dijkstra's PQ method

--- Graphs/shortestPath/DijkstrasAlgorithm_priorityQueue.cpp
+++ Graphs/shortestPath/DijkstrasAlgorithm_priorityQueue.cpp
@@ -12,23 +12,39 @@ vector<int> dijsktraPQ(vector<vector<pair<int,int>>> &adjList,int src)
     dist[src]=0;
     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
     pq.push({0,src});
-    while(!pq.empty())
+    while(!pq.empty()) // TC - o(V)
     {
-        int dNode=pq.top().first;
+        int dNode=pq.top().first;//TC - O(log( heap size))
         int node=pq.top().second;
         pq.pop();
-        for(auto &[x,d]:adjList[node])
+        for(auto &[x,d]:adjList[node])//TC - O(number of edges (ne = V-1))
         {
             if(dist[x]>d+dNode)
             {
                 dist[x]=d+dNode;
-                pq.push({dist[x],x});
+                pq.push({dist[x],x});// TC - O(log (heap size))
             }
         }
     }
     return dist;
 }
 
+//TC derivation
+/*
+O ( V * ((pop in min heap)) + (number of egdes)*(push in PQ)))
+O ( V * (log(heap size) + ne*log(heap size)))
+O ( V * (log(heap size) + (V-1)*log(heap size)))
+
+heap size ~~ V^2
+
+O ( V * V * log(V^2))
+O (2 * V^2 * log(V))
+
+ne = E = V^2
+
+O(E * log (V))
+
+*/
 
 
 int main()

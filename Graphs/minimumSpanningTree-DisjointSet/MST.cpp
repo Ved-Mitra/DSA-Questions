76b02f0523a2b23e44f0f9473d36c3b8a01b0b30
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-11-09 18:13:33 +0530

    adding intro to what is MST

---
+++ Graphs/minimumSpanningTree-DisjointSet/MST.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//undirected weighted graph
+// a tree in which  we have N nodes and N-1 edges and all nodes should be reachable from each other
+//the ST made from a graph which has the minimum sum of weights is called MST
+//graph can have any spanning tree
+//ony one MST
+
+
+vector<vector<int>> minimumSpanningTree(vector<vector<int>> &adjList,int n)
+{
+    //BY:
+    //Prim's algorithm
+    //Krushkal's Algorithm
+}
+
+
+int main()
+{
+
+}

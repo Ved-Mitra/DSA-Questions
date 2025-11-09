6cfdde4ec35e27a1c3166dda2ad40b15fcd6106d
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-11-10 01:22:41 +0530

    adding Disjoint set data structure code

---
+++ Graphs/minimumSpanningTree-DisjointSet/DisjointSet.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//V.V.V important
+//used in Dynamic graph
+//does what BFS or DFS does in O(1)
+
+//gives findParent() , Union()(by rank or size)
+
+//1. find ultimate parent of u and v
+//2. find rank of ultimate parent
+//3. connect smaller rank to larger rank
+
+//TC - O(4 * alpha) alpha ~ 1
+//See mathematic derivation of alpha from internet
+
+class DisjointSet//union by rank
+{
+    private:
+    vector<int> rank,parent;
+    public:
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+            parent[i]=i;
+    }
+
+    int findParent(int node)
+    {
+        if(node==parent[node])
+            return node;
+        return parent[node]=findParent(parent[node]);
+    }
+
+    void unionByRank(int u,int v)
+    {
+        int ultimateParent_u=findParent(u);
+        int ultimateParent_v=findParent(v);
+        if(ultimateParent_u=ultimateParent_v)
+            return;
+        if(rank[ultimateParent_u]>rank[ultimateParent_v])
+            parent[ultimateParent_v]=ultimateParent_u;
+        else if(rank[ultimateParent_u]<rank[ultimateParent_v])
+            parent[ultimateParent_u]=ultimateParent_v;
+        else
+        {
+            //if same then attach v-->u
+            parent[ultimateParent_v]=ultimateParent_u;
+            rank[ultimateParent_u]++;
+        }
+    }
+};
+
+
+class DisjointSet//union by size
+{
+    private:
+    vector<int> size,parent;
+    public:
+    DisjointSet(int n)
+    {
+        size.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+            parent[i]=i;
+    }
+
+    int findParent(int node)
+    {
+        if(node==parent[node])
+            return node;
+        return parent[node]=findParent(parent[node]);
+    }
+
+    void unionBySize(int u,int v)
+    {
+        int ultimateParent_u=findParent(u);
+        int ultimateParent_v=findParent(v);
+        if(ultimateParent_u=ultimateParent_v)
+            return;
+        if(ultimateParent_u=ultimateParent_v)
+            return;
+        if(size[ultimateParent_u]<size[ultimateParent_v])
+        {
+            parent[ultimateParent_u]=ultimateParent_v;
+            size[ultimateParent_v]+=size[ultimateParent_u];
+        }
+        else
+        {
+            //if same then attach v-->u
+            parent[ultimateParent_v]=ultimateParent_u;
+            size[ultimateParent_u]+=size[ultimateParent_v];
+        }
+    }
+};
+
+//use either union by rank or size
+
+
+int main()
+{
+
+}

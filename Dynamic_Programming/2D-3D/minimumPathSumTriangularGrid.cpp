032474cc5ad999915b634562be279deee159f4de
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-12-04 17:51:46 +0530

    adding question to get minimum triangular grid path

--- Dynamic_Programming/2D-3D/minimumPathSumTriangularGrid.cpp
+++ Dynamic_Programming/2D-3D/minimumPathSumTriangularGrid.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int minimumTotal_Memoization(vector<vector<int>> &triangle,int r,int c)
+int minimumTotal_Memoization(vector<vector<int>> &triangle,int r,int c,vector<vector<int>> &dp)
 {
-    if(r>=triangle.size())  return 0;
-    int down=triangle[r][c]+minimumTotal_Memoization(triangle,r+1,c);
-    int diagonal=triangle[r][c]+minimumTotal_Memoization(triangle,r+1,c+1);
-    return min(down,diagonal);
+    if(r==triangle.size()-1)
+        return triangle[triangle.size()-1][c];
+    if(dp[r][c]!=-1)
+        return dp[r][c];
+    int down=triangle[r][c]+minimumTotal_Memoization(triangle,r+1,c,dp);
+    int dg=triangle[r][c]+minimumTotal_Memoization(triangle,r+1,c+1,dp);
+    return dp[r][c]=min(down,dg);
 }
 
 int minimumTotal_Tabulation(vector<vector<int>> &triangle)

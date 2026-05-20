25d1a8554e9f2fcba066160ad104478009894290
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-05-20 16:33:06 +0530

    adding new problem of minimum multiplication

--- Graphs/shortestPath/minimumMultiplication.cpp
+++ Graphs/shortestPath/minimumMultiplication.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//start with start then reach end by mutiplying by any number in array then mod 100000
+//Given start, end and an array arr of n numbers. At each step, start is multiplied with any number in the array and then mod operation with 100000 is done to get the new start. Your task is to find the minimum steps in which end can be achieved starting from start. If it is not possible to reach end, then return -1.
 
 int minimumMultiplication(vector<int> &arr,int start,int end)
 {
-    
+    if(start==end)
+        return 0;
+    int n=arr.size();
+    queue<pair<int,int>>pq;
+    pq.push({0,start});
+    int mod=1e5;
+    vector<bool> visited(mod+1,false);
+    visited[start]=true;
+    while(!pq.empty())
+    {
+        int val=pq.front().second;
+        int steps=pq.front().first;
+        pq.pop();
+        for(int i=0;i<n;i++)
+        {
+            int mul=(val*arr[i])%mod;
+            if(mul==end)
+                return steps+1;
+            if(!visited[mul])
+            {
+                visited[mul]=true;
+                pq.push({steps+1,mul});
+            }
+        }
+    }
+    return -1;
 }
 
 

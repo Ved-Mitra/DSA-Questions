eab88b42921d98f37341406d9c3fc27048f14284
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-06-21 23:48:12 +0530

    Refactor median finding logic in sorted arrays

--- Binary Search/median_sorted_array.cpp
+++ Binary Search/median_sorted_array.cpp
@@ -4,6 +4,43 @@ using namespace std;
 /*
 Q. areturn the median of merged sorted array
 */
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+{
+    int n1=nums1.size();
+    int n2=nums2.size();
+    if(n1>n2)
+        return findMedianSortedArrays(nums2,nums1);
+
+    int low=0,high=n1;
+    int n=(n1+n2+1)/2;
+    while(low<=high)
+    {
+        int cut1=(low+high)/2;
+        int cut2= n-cut1;
+        int l1 = cut1-1<0 ? INT_MIN : nums1[cut1-1];
+        int l2 = cut2-1<0 ? INT_MIN : nums2[cut2-1];
+        int r1 = cut1>=n1 ? INT_MAX : nums1[cut1];
+        int r2 = cut2>=n2 ? INT_MAX : nums2[cut2];
+        if(l1>r2)
+        {
+            high=cut1-1;
+        }
+        else if(l2>r1)
+        {
+            low=cut1+1;
+        }
+        else
+        {
+            if((n1+n2)%2==0)
+                return (max(l1,l2) + min(r1,r2))/2.0;
+            else
+                return max(l1,l2);
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     int n1,n2;
@@ -19,38 +56,5 @@ int main()
         cin >> nums2[i];
     
 
-    int low=0,high=n1;
-    int left=(n1+n2+1)/2;
-    int n=n1+n2;
-    if(n2>n1)
-    {
-        swap(nums1,nums2);
-    }
-    while(low<=high)
-    {
-        int mid1=(low+high)/2;
-        int mid2=left-mid1;
-        int l1=INT_MIN, l2=INT_MIN;
-        int r1=INT_MAX, r2=INT_MAX;
-        if(mid1<n1)
-            r1=nums1[mid1];
-        if(mid2<n2)
-            r2=nums2[mid2];
-        if(mid1-1 >=0)
-            l1=nums1[mid1-1];
-        if(mid2-1>=0)
-            l2=nums2[mid2-1];
-        if(l1<=r2 && l2<=r1)
-        {
-            if(n%2==1)
-                return max(l1,l2);
-            return ((double)max(l1,l2) + min(r1,r2))/ 2.0;
-        }
-        else if(l1>r2)
-            high=mid1-1;
-        else
-            low=mid1+1;
-    }
-    
-    return 0;
+    cout << findMedianSortedArrays(nums1,nums2) << '\n';
 }

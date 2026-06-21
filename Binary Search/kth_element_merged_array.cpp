38683b86fe1a044591d7657b4ed8547abe5095a3
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-06-21 23:51:50 +0530

    Implement kth element in merged sorted arrays

--- Binary Search/kth_element_merged_array.cpp
+++ Binary Search/kth_element_merged_array.cpp
@@ -4,6 +4,41 @@ using namespace std;
 /*
 Q. return the kth of merged sorted array
 */
+int kthElement(vector<int> &a, vector<int> &b, int k) 
+{
+        // code here
+    int n1=a.size();
+    int n2=b.size();
+    if(n1>n2)
+        return kthElement(b,a,k);
+
+    int low = max(0, k - n2);
+    int high = min(k, n1);
+    
+    while(low<=high)
+    {
+        int cut1=(low+high)/2;
+        int cut2= k-cut1;
+        int l1 = cut1-1<0 ? INT_MIN : a[cut1-1];
+        int l2 = cut2-1<0 ? INT_MIN : b[cut2-1];
+        int r1 = cut1>=n1 ? INT_MAX : a[cut1];
+        int r2 = cut2>=n2 ? INT_MAX : b[cut2];
+        if(l1>r2)
+        {
+            high=cut1-1;
+        }
+        else if(l2>r1)
+        {
+            low=cut1+1;
+        }
+        else
+        {
+            return max(l1,l2);
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     int n1,n2;
@@ -20,37 +55,6 @@ int main()
     int k;
     cout << "Enter the kth position: " << endl;
     cin >> k;
-    
-    int low=max(0,k-n2),high=min(k,n1);
-    int left=k;
-    int n=n1+n2;
-    if(n2>n1)
-    {
-        swap(nums1,nums2);
-    }
-    while(low<=high)
-    {
-        int mid1=(low+high)/2;
-        int mid2=left-mid1;
-        int l1=INT_MIN, l2=INT_MIN;
-        int r1=INT_MAX, r2=INT_MAX;
-        if(mid1<n1)
-            r1=nums1[mid1];
-        if(mid2<n2)
-            r2=nums2[mid2];
-        if(mid1-1 >=0)
-            l1=nums1[mid1-1];
-        if(mid2-1>=0)
-            l2=nums2[mid2-1];
-        if(l1<=r2 && l2<=r1)
-        {
-            return max(l1,l2);
-        }
-        else if(l1>r2)
-                high=mid1-1;
-        else
-            low=mid1+1;
-    }
-    
-    return 0;
+
+    cout << kthElement(nums1,nums2) << '\n';
 }
